bd8a7d37f2f173dd7eab4e0ff436bee7e9bcd21e
Coelacanth7 <galford151@gmail.com>
2018-02-27 17:52:42 -0700

    bubble

---
+++ bubbleSort.js
@@ -0,0 +1,23 @@
+//
+
+function bubbleSort(arr) {
+	let swapped, temp;
+
+	do {
+		swapped = false;
+		for (let i = 0; i < arr.length - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
+			}
+		}
+	} while (swapped);
+
+	return arr;
+}
+
+console.log(bubbleSort([1, 3, 7, 2, 5]));
+console.log(bubbleSort([9, 6, 7, 5, 4, 3, 2, 1, 666]));
+console.log(bubbleSort([6, 7, 85, 4, 3, -3, -2, -1]));
